923b997e4eb60279f482d3e9cee79700766c6a02
lokesh-sarode <sarodelokesh005@gmail.com>
2025-07-31 12:24:15 +0530

    Initial Update

---
+++ index.js
@@ -0,0 +1,9 @@
+function bigImg(img) {
+    img.style.height = "400px";
+    img.style.width = "400px";
+}
+
+function normalImg(img) {
+    img.style.height = "100px";
+    img.style.width = "100px";
+}
