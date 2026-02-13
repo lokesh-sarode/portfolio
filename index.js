77f2a66da42c722ad6a1321de6c277f9936d6d17
Lokesh Sarode <lokesh.sarode@ARKENTECH.IN>
2026-02-14 03:35:04 +0530

    Updated the sections and home page content

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-function bigImg(img) {
-    img.style.height = "400px";
-    img.style.width = "400px";
-}
+const text = [
+    "Web Developer",
+    "Python Developer",
+    "Cybersecurity Specialist",
+    "VAPT Engineer"
+]
 
-function normalImg(img) {
-    img.style.height = "100px";
-    img.style.width = "100px";
+let count = 0;
+let index = 0;
+
+let currentText = "";
+let letter = "";
+
+(function type() {
+    if (count === text.length) count = 0;
+
+    currentText = text[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector(".typing").textContent = letter;
+
+    if (letter.length === currentText.length) {
+        setTimeout(() => {
+            index = 0;
+            count++;
+        }, 1000);
+    }
+    setTimeout(type, 200);
 }
+)();
+
+
+const donwloadBtn = document.getElementById(".downloadBtn");
+donwloadBtn.addEventListener("click", () => {
+    document.getElementById("popup").classList.toggle(".active");
+})
