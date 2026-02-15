2cab9a63e0931b6bb0e0bd3d234116b6cde1edfa
lokesh-sarode <sarodelokesh005@gmail.com>
2026-02-16 01:36:44 +0530

    Updated Download Resume Popup

---
+++ index.js
@@ -0,0 +1,48 @@
+const text = [
+    "Web Developer",
+    "Python Developer",
+    "Cybersecurity Specialist",
+    "VAPT Engineer"
+]
+
+let count = 0;
+let index = 0;
+
+let currentText = "";
+let letter = "";
+
+(function type() {
+    if (count === text.length) count = 0;
+
+    currentText = text[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector(".typing").textContent = letter;
+
+    if (letter.length === currentText.length) {
+        setTimeout(() => {
+            index = 0;
+            count++;
+        }, 1000);
+    }
+    setTimeout(type, 200);
+}
+)();
+
+
+
+const donwloadBtn = document.getElementById("downloadBtn");
+const popup = document.getElementById("popup");
+
+donwloadBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    popup.classList.toggle("active");
+})
+
+popup.addEventListener("click", (e) => {
+    e.stopPropagation();
+})
+// closing popup
+document.addEventListener("click", () => {
+    popup.classList.remove("active");
+});
