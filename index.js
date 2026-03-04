1859f41d80d381d664f090420fd7abd602331a84
Lokesh Sarode <lokesh.sarode@ARKENTECH.IN>
2026-03-05 04:31:17 +0530

    progress-bar updated

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+window.addEventListener("scroll", () => {
+    const scrollTop = window.scrollY;
+    const docHeight = document.body.scrollHeight - window.innerHeight;
+    const scrollPercent = (scrollTop / docHeight) * 100;
+
+    document.querySelector(".progress-bar").style.width = scrollPercent + "%";
+})
+
 
 // Scroll reveal animation
 document.addEventListener("DOMContentLoaded", function () {
